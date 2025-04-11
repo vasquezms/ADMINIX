d5cf914117acee92ddd39c905d642ca1d8d00b38
DESKTOP-7ST3MPC\Janus <Mafe>
2025-04-11 03:49:14 -0500

    Frontend

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Http\Controllers\CategoriaController;
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
+
+require __DIR__.'/auth.php';
